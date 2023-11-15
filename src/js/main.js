523a07ef226be48c86672783b60f979b1fbda1c6
jenniferericson <jenniferericson813@gmail.com>
2023-11-15 13:47:22 +0100

    created objects and list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import "./../scss/style.scss"
+import { Skincare } from "./modules/skincare"
+
+const productOne = new Skincare("Paulas Choice", "BHA", "100 ml", 599);
+
+const productTwo = new Skincare("Aco", "Moisturizer", "50 ml", 299);
+
+const productThree = new Skincare("Klairs", "Toner", "100 ml", 399);
+
+const productFour = new Skincare("Medik8", "Serum", "30 ml", 799);
+
+const products = [productOne,productTwo,productThree,productFour];
+
---
+++ src/js/modules/skincare.js
@@ -0,0 +1,13 @@
+export class Skincare {
+    brand;
+    type;
+    size;
+    price;
+
+    constructor (brand,type,size,price) {
+        this.brand = brand;
+        this.type = type;
+        this.size = size;
+        this.price = price;
+    }
+}
