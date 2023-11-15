c82db735f8aa8265b19b157a030e3b313e34c9dd
jenniferericson <jenniferericson813@gmail.com>
2023-11-15 14:29:22 +0100

    added styling for my products

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,29 @@ const productFour = new Skincare("Medik8", "Serum", "30 ml", 799);
 
 const products = [productOne,productTwo,productThree,productFour];
 
+const divApp = document.getElementById("app");
+
+for(let i = 0; i < products.length; i++) {
+
+  let brandTag = document.createElement("h3");
+  let typeTag = document.createElement("h4");
+  let sizeTag = document.createElement("p");
+  let priceTag = document.createElement("p");
+  const container = document.createElement("div");
+
+  brandTag.innerHTML = products[i].brand;
+  typeTag.innerHTML = products[i].type;
+  sizeTag.innerHTML = products[i].size;
+  priceTag.innerHTML = products[i].price;
+  
+  divApp.appendChild(container);
+  container.appendChild(brandTag);
+  container.appendChild(typeTag);
+  container.appendChild(sizeTag);
+  container.appendChild(priceTag);
+  
+  container.className = "productContainer";
+}
+
+
+
