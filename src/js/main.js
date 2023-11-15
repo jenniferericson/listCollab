ee12e49f8549fbd632ffa59e7e8fb5b7f4d8ae72
Andreas Retzius <andreasretzius@gmail.com>
2023-11-15 13:47:36 +0100

    start of my list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import "./../scss/style.scss"
+import { Cat } from "./modules/Cat"
+
+const cat1 = new Cat("Lizie","Barn Cat","Black","Yellow");
+const cat2 = new Cat("Disney","Maine Coon","White","Blue");
+const cat3 = new Cat("Zoey", "Norwegian Forest Cat", "Grey", "Yellow");
+const cat4 = new Cat("Sally", "Maine Coon", "Grey", "Yellow");
+const cat5 = new Cat("Zelda", "Barn Cat", "White and Black", "Yellow");
+
+const cats = [cat1,cat2,cat3,cat4,cat5];
---
+++ src/js/modules/Cat.js
@@ -0,0 +1,20 @@
+export class Cat{
+    name;
+    breed;
+    furColor;
+    eyeColor;
+
+    constructor(name, breed, furColor, eyeColor) {
+        this.name = name;
+        this.breed = breed;
+        this.furColor = furColor;
+        this.eyeColor = eyeColor;
+    }
+
+
+}
+/*
+function Meow(){
+    console.log("MEOW my name is " + name);
+}
+*/
